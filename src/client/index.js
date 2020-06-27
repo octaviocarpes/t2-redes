fe97b1d6341edf16692274a2cc23d97bcf34bbae
Octavio Carpes <octavio.carpes10@gmail.com>
2020-06-27 18:41:32 -0300

    Adding client logic

--- src/client/index.js
+++ src/client/index.js
@@ -1,28 +1,62 @@
 import client from './client.js';
 import readFile from '../util/readFile.js';
 const SERVER_PORT = 41234;
+const SERVER_ADDRESS = '189.6.202.186';
 
-const slowStart = () => {
-  console.log('Slow start...');
-};
+let lastAck = 0;
+let offset = 0;
+let slowStartSize = 1;
 
-const fastRetransmit = () => {
-  console.log('Fast retransmit...');
-};
+const file = readFile('src/client/text.txt'); // 3008
+const fileSize = file.length;
 
-const uploadFileToServer = () => {
-  const file = readFile('src/client/text.txt'); // 3008
-  // begin slow start
-  slowStart();
-  // begin fast retransmit
-  fastRetransmit();
-  const offset = 0;
-  const length = 1500;
-  client.send(file, offset, length)
+const sendData = () => {
+  return new Promise((resolve, reject) => {
+    client.send(file, offset, 1471, error => {
+      if (error) {
+        reject();
+      } else resolve();
+    });
+  });
 }
 
-client.connect(SERVER_PORT, () => {
-  console.log('client connected!');
-  console.log('Sending file...');
-  uploadFileToServer();
+const sequencialSend = async count => {
+  for (let i = 1; i <= count; i++) {
+    if (offset < fileSize) {
+      console.log('Sending sequencial', i);
+      await sendData();
+      offset += 1471;
+    }
+  };
+};
+
+client.on('message', (msg, rinfo) => {
+    console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+    // if ack for igual ao anterior troca pra 1 e comeca dnv
+    const ack = parseInt(`${msg}`.split('-')[1]);
+
+    if (ack === file.length) return;
+
+    if (ack === offset) {
+      slowStartSize = 1;
+    } else {
+      if (offset === 0) {
+        offset = ack;
+        slowStartSize = slowStartSize * 2;
+        sequencialSend(slowStartSize);
+      }
+      slowStartSize = slowStartSize * 2;
+    };
+
+    console.log(offset, ack, slowStartSize);
+
+    if (ack === (slowStartSize * 1471 + 1471)) {
+      return
+    }
+    sequencialSend(slowStartSize);
+});
+
+client.connect(SERVER_PORT, SERVER_ADDRESS, () => {
+  console.log('Client connected!');
+  sendData();
 });
